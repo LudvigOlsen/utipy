faa04d820a3f291c8ef523c0d2d2c7a6df405066
Ludvig Renbo Olsen <mail@ludvigolsen.dk>
2017-04-23 02:56:25 +0200

    Added helper for conversion
    For converting data input to pd.DataFrame. Very specific to distort()
    at the moment. Subject to change.

---
+++ utipy/helpers/convert_to_df.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 29 15:16:39 2017
+
+@author: ludvigolsen
+"""
+
+import warnings
+import numpy as np
+import pandas as pd
+
+def convert_to_df(data):
+    """
+    Checks the type of data
+    If it is not a pd.DataFrame it 
+    attempts to convert to pd.DataFrame
+    
+    """
+    
+    data_type = 'pd.DataFrame'
+    if isinstance(data, pd.DataFrame):
+        return(data, data_type)
+
+    elif isinstance(data, pd.Series):
+        data_type = 'pd.Series'
+        data = pd.DataFrame({'x':data})
+
+    elif type(data).__module__ == np.__name__:
+        
+        # warnings.warn("'data' is a numpy object. Will attempt conversion to pandas.DataFrame")
+        try:
+            data = pd.DataFrame(data)
+            data_type = 'np.ndarray'
+
+        except:
+            raise TypeError("'data' is the wrong format.")      
+    else:
+        raise TypeError("'data' is the wrong format.")
+        
+            
+    return(data, data_type)
