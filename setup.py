f3e6eabbec339b497207a6ad6a1b745339ee8f2a
Ludvig Renbo Olsen <mail@ludvigolsen.dk>
2017-05-07 17:49:35 +0200

    New release - versioning

--- setup.py
+++ setup.py
@@ -11,15 +11,16 @@ def readme():
         return f.read()
 
 setup(name='utipy',
-      version='0.1.3',
+      version='0.2.0',
       description='Utility functions for python',
-      long_description='Pandas operations. Data grouping, folding, and partitioning.',
+      long_description='Pandas and array operations. Data grouping, folding, and partitioning.',
       classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
       ],
-      keywords='pandas groups folds kfold partitioning utilities tools',
+      keywords='pandas numpy array ndarray groups folds kfold partitioning utilities tools',
       url='http://github.com/ludvigolsen/utipy',
       author='Ludvig Renbo Olsen',
       author_email='mail@ludvigolsen.dk',
