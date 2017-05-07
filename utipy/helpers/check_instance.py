6eb28d830afe70f4a5866b6f4256e242c07817bf
Ludvig Renbo Olsen <mail@ludvigolsen.dk>
2017-05-07 14:43:31 +0200

    Added helper tools for instance types
    Check and convert

---
+++ utipy/helpers/check_instance.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env 
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 7 13:50:39 2017
+
+@author: ludvigolsen
+"""
+
+import numpy as np
+import pandas as pd
+
+def check_instance(data):
+    """
+    Checks if data is either:
+        list
+        tuple
+        np.ndarray
+        pd.Series
+        pd.DataFrame
+    
+    """
+    if isinstance(data, list):
+        return('list')
+    elif isinstance(data, tuple):
+        return('tuple')
+    elif type(data).__module__ == np.__name__:
+        return('np.ndarray')
+    elif isinstance(data, pd.Series):
+        return('pd.Series')
+    elif isinstance(data, pd.DataFrame):
+        return('pd.DataFrame')
+    else:
+        raise(TypeError("Doesn't recognize instance type"))
---
+++ utipy/helpers/convert_to_type.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env 
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 7 13:50:39 2017
+
+@author: ludvigolsen
+"""
+
+import numpy as np
+import pandas as pd
+
+def convert_to_type(data, data_type):
+    
+    """
+    Converts (if necessary) data into either of these data types:
+    list
+    tuple
+    np.ndarray
+    pd.Series
+    pd.DataFrame
+    
+    """
+    if data_type == 'list' and not isinstance(data, list):
+        try:
+            return(list(data))
+        except:
+            raise(TypeError("Could not convert to list"))
+    
+    elif data_type == 'tuple' and not isinstance(data, tuple):
+        try:
+            return(tuple(data))
+        except:
+            raise(TypeError("Could not convert to tuple"))
+    
+    elif data_type == 'np.ndarray' and type(data).__module__ != np.__name__:
+        try:
+            return(np.asarray(data))
+        except:
+            raise(TypeError("Could not convert to numpy.ndarray"))
+            
+    elif data_type == 'pd.Series' and not isinstance(data, pd.Series):
+        try:
+            return(pd.Series(data))
+        except:
+            raise(TypeError("Could not convert to pandas.Series"))
+    
+    elif data_type == 'pd.DataFrame' and not isinstance(data, pd.DataFrame):
+        try:
+            try:
+                return(pd.DataFrame(data))
+            except:
+                return(pd.DataFrame({'x':data}))
+        except:
+            raise(TypeError("Could not convert to pandas.DataFrame"))
+    else:
+        return(data)
+        
