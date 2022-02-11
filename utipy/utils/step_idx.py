8a3d0689e886b593c1cc9443eb2c012e3cd266ab
Ludvig <ludvig@clin.au.dk>
2022-02-11 18:41:00 +0100

    Add step_idx utility

---
+++ utipy/utils/step_idx.py
@@ -0,0 +1,20 @@
+
+# TODO Add documentation and tests
+
+class StepIdx:
+
+    def __init__(self, start_idx=0) -> None:
+        self.current = start_idx
+
+    def step(self):
+        self.current += 1
+
+    @property
+    def previous(self):
+        if self.current == 0:
+            raise ValueError("`current` was 0 which has no `previous` index.")
+        return self.current - 1
+
+    @property
+    def next(self):
+        return self.current + 1
