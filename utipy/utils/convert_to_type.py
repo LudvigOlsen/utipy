9f5d37a0cf5ffc5dbc988092e37857052f64b276
Ludvig <ludvig@clin.au.dk>
2022-02-11 18:10:12 +0100

    Remove try-catch as error messages are worse than those given naturally

--- utipy/utils/convert_to_type.py
+++ utipy/utils/convert_to_type.py
@@ -9,45 +9,29 @@ def convert_to_type(data, data_type):
     
     """
     Converts (if necessary) data into either of these data types:
-    list
-    tuple
-    np.ndarray
-    pd.Series
-    pd.DataFrame
+     - list
+     - tuple
+     - np.ndarray
+     - pd.Series
+     - pd.DataFrame
     
     """
+    if data_type not in ["list", "tuple", "np.ndarray", "pd.Series", "pd.DataFrame"]:
+        raise ValueError(f"`data_type` was unknown: '{data_type}'.")
+
+    # Convert 
     if data_type == 'list' and not isinstance(data, list):
-        try:
-            return list(data)
-        except:
-            raise TypeError("Could not convert to list")
-    
+        return list(data)
     elif data_type == 'tuple' and not isinstance(data, tuple):
-        try:
-            return tuple(data)
-        except:
-            raise TypeError("Could not convert to tuple")
-    
+        return tuple(data)
     elif data_type == 'np.ndarray' and type(data).__module__ != np.__name__:
-        try:
-            return np.asarray(data)
-        except:
-            raise TypeError("Could not convert to numpy.ndarray")
-            
+        return np.asarray(data)    
     elif data_type == 'pd.Series' and not isinstance(data, pd.Series):
-        try:
-            return pd.Series(data)
-        except:
-            raise TypeError("Could not convert to pandas.Series")
-    
+        return pd.Series(data)
     elif data_type == 'pd.DataFrame' and not isinstance(data, pd.DataFrame):
         try:
-            try:
-                return pd.DataFrame(data)
-            except:
-                return pd.DataFrame({'x':data})
+            return pd.DataFrame(data)
         except:
-            raise TypeError("Could not convert to pandas.DataFrame")
-    else:
-        return data
+            return pd.DataFrame({'x':data})
+    return data
         
