addfacf8393ab06e3c0ff0fa10c48ef5bb796a3e
Ludvig <ludvig@clin.au.dk>
2022-02-11 17:22:02 +0100

    Reformatting plus copy arg in groups methods

--- utipy/groups/group_uniques.py
+++ utipy/groups/group_uniques.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env 
-# -*- coding: utf-8 -*-
 """
 @author: ludvigolsen
 """
@@ -8,35 +6,35 @@ import pandas as pd
 from .methods.l_sizes import _l_sizes
 from .methods.n_dist import _n_dist
 
-def group_uniques(data, n, col, method = 'n_dist'):
-    """
-    Adds grouping factor to given
-    dataframe by unique values in col.
-    
-    """
 
-    data = data.copy()
-    
+def group_uniques(data, n, col, method='n_dist', copy=True):
+    """
+    Add grouping factor to given
+    dataframe by unique values in col.
+
+    """
+    if copy:
+        data = data.copy()
+
     # Get unique IDs
     uniques = list(set(data[col]))
 
     # Create grouping factor from unique IDs
     if method == 'n_dist':
-        all_group_ids = _n_dist(uniques, n, randomize = True)
+        all_group_ids = _n_dist(uniques, n, randomize=True)
     elif method == 'l_sizes':
-        all_group_ids = _l_sizes(uniques, n, randomize = True)
-    
+        all_group_ids = _l_sizes(uniques, n, randomize=True)
+
     # Create dictionary with unique values
     # and groups
     groups_dict = {}
     groups_dict[col] = uniques
     groups_dict['group'] = all_group_ids
-    
+
     # Create dataframe with unique IDs and the
     # grouping factor
     df_grouped = pd.DataFrame(groups_dict)
-    
+
     # Merge and return the given dataframe with the
     # new grouping factor
-    return(data.merge(df_grouped, on = col, how='outer'))
-    
+    return data.merge(df_grouped, on=col, how='outer')
--- utipy/groups/methods/n_dist.py
+++ utipy/groups/methods/n_dist.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env 
-# -*- coding: utf-8 -*-
 """
 @author: ludvigolsen
 """
@@ -7,28 +5,27 @@
 import numpy as np
 from random import shuffle
 
-def _n_dist(v, n, randomize = False):
+
+def _n_dist(v, n, randomize=False):
     """
     Creates grouping factor with
     distributed excess elements
-    
+
     """
     len_v = float(len(v))
-    
-    divisor = len_v/n
-    
+
+    divisor = len_v / n
+
     # Create range the length of v (+1 because it starts at 0)
     # Divide each element and get everything but the first element (0)
-    v_divided = [vi / divisor for vi in xrange(int(len_v)+1)][1:]
-    
+    v_divided = [vi / divisor for vi in range(int(len_v) + 1)][1:]
+
     # First round to a smaller decimal number to avoid ceil of
     # e.g. 7.00000000...03 becoming 8
     # Then round up to nearest integer
     v_ceiled = np.ceil(np.around(v_divided, 5)).astype(int)
-    
+
     if randomize:
-        
         shuffle(v_ceiled)
-    
+
     return(v_ceiled)
-  
