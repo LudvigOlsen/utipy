c6b2a08a4ccbf39138518ad962709e03cc53086a
Ludvig <ludvig@clin.au.dk>
2022-02-11 17:02:08 +0100

    Reformats test files

--- tests/test_pandas_drop.py
+++ tests/test_pandas_drop.py
@@ -4,164 +4,166 @@ import utipy as ut
 import numpy as np
 import pandas as pd
 
+
 def test_drop_DF_columns():
 
-    df = pd.DataFrame({'a': [1,2,3,4,5],
-                       'b': [2,3,4,5,6],
-                       'c': ['a','b','c','d','e'],
-                       's': ['a','a','a','b','c'],
-                       'na': [np.nan, np.nan, 3,4,1],
-                       'infs': [np.inf, np.nan, 3,4,1],
-                       'zero': [0,0,0,0,1],
-                       'zeros': [0,0,0,0,0]},
-                       columns = ['a', 'b','c', 's','na','infs','zero','zeros'])
-    
+    df = pd.DataFrame({'a': [1, 2, 3, 4, 5],
+                       'b': [2, 3, 4, 5, 6],
+                       'c': ['a', 'b', 'c', 'd', 'e'],
+                       's': ['a', 'a', 'a', 'b', 'c'],
+                       'na': [np.nan, np.nan, 3, 4, 1],
+                       'infs': [np.inf, np.nan, 3, 4, 1],
+                       'zero': [0, 0, 0, 0, 1],
+                       'zeros': [0, 0, 0, 0, 0]},
+                      columns=['a', 'b', 'c', 's', 'na', 'infs', 'zero', 'zeros'])
+
     # Check one or two dataframes, then just check the colnames
-    
 
     # Columns
 
-    droppedNaN = ut.drop(data = df, 
-                         value = 'NaN',
-                         thresh = 0,
-                         direction = '>')
+    droppedNaN = ut.drop(data=df,
+                         value='NaN',
+                         thresh=0,
+                         direction='>')
 
-    assert (droppedNaN.columns == ['a', 'b','c', 's','zero','zeros']).all()
+    assert (droppedNaN.columns == ['a', 'b', 'c', 's', 'zero', 'zeros']).all()
 
+    droppedinf = ut.drop(data=df,
+                         value='inf',
+                         thresh=0,
+                         direction='>')
 
-    droppedinf = ut.drop(data = df, 
-                         value = 'inf',
-                         thresh = 0,
-                         direction = '>')
+    assert (droppedinf.columns == [
+            'a', 'b', 'c', 's', 'na', 'zero', 'zeros']).all()
 
-    assert (droppedinf.columns == ['a', 'b','c', 's', 'na', 'zero','zeros']).all()
+    dropped0 = ut.drop(data=df,
+                       value=0,
+                       thresh=0.9,
+                       direction='>')
 
-    dropped0 = ut.drop(data = df, 
-                         value = 0,
-                         thresh = 0.9,
-                         direction = '>')
+    assert (dropped0.columns == ['a', 'b', 'c',
+            's', 'na', 'infs', 'zero']).all()
 
-    assert (dropped0.columns == ['a', 'b','c', 's','na','infs','zero']).all()
+    dropped08 = ut.drop(data=df,
+                        value=0,
+                        thresh=0.8,
+                        direction='>=')
 
-    dropped08 = ut.drop(data = df, 
-                         value = 0,
-                         thresh = 0.8,
-                         direction = '>=')
+    assert (dropped08.columns == ['a', 'b', 'c', 's', 'na', 'infs']).all()
 
-    assert (dropped08.columns == ['a', 'b','c', 's','na','infs']).all()
+    droppedany = ut.drop(data=df,
+                         value='any',
+                         thresh=0.3,
+                         direction='>')
 
-    droppedany = ut.drop(data = df, 
-                         value = 'any',
-                         thresh = 0.3,
-                         direction = '>')
+    assert (droppedany.columns == ['a', 'b', 'c', 'infs']).all()
 
-    assert (droppedany.columns == ['a', 'b','c','infs']).all()
+    droppednone = ut.drop(data=df,
+                          value=7,
+                          thresh=1,
+                          direction='>')
 
-    droppednone = ut.drop(data = df, 
-                         value = 7,
-                         thresh = 1,
-                         direction = '>')
-
-    assert (droppednone.columns == ['a', 'b','c', 's','na','infs','zero','zeros']).all()
+    assert (droppednone.columns == [
+            'a', 'b', 'c', 's', 'na', 'infs', 'zero', 'zeros']).all()
     assert (droppednone.dtypes == df.dtypes).all()
     # exclude
-    droppedexc = ut.drop(data = df, 
-                         value = 'NaN',
-                         thresh = 0,
-                         direction = '>',
-                         exclude = ['infs'])
+    droppedexc = ut.drop(data=df,
+                         value='NaN',
+                         thresh=0,
+                         direction='>',
+                         exclude=['infs'])
 
-    assert (droppedexc.columns == ['a', 'b','c', 's','infs','zero','zeros']).all()
+    assert (droppedexc.columns == ['a', 'b', 'c',
+            's', 'infs', 'zero', 'zeros']).all()
 
     # include
-    droppedinc = ut.drop(data = df, 
-                         value = 'NaN',
-                         thresh = 0,
-                         direction = '>',
-                         include = ['infs'])
+    droppedinc = ut.drop(data=df,
+                         value='NaN',
+                         thresh=0,
+                         direction='>',
+                         include=['infs'])
 
-    assert (droppedinc.columns == ['a', 'b','c', 's','na','zero','zeros']).all()
-
+    assert (droppedinc.columns == [
+            'a', 'b', 'c', 's', 'na', 'zero', 'zeros']).all()
 
 
 def test_drop_DF_rows():
 
-    df = pd.DataFrame({'a': [np.inf,np.nan,3,4,5],
-                       'b': [2,3,4,5,6],
-                       'c': ['a','b','c','d','e'],
-                       's': ['a','a','a','b','c'],
-                       'na': [np.nan, np.nan, 3,4,1],
-                       'infs': [np.inf, np.nan, 3,4,1],
-                       'zero': [0,0,0,0,1],
-                       'zeros': [0,0,0,0,0]},
-                       columns = ['a', 'b','c', 's','na','infs','zero','zeros'])
-    
+    df = pd.DataFrame({'a': [np.inf, np.nan, 3, 4, 5],
+                       'b': [2, 3, 4, 5, 6],
+                       'c': ['a', 'b', 'c', 'd', 'e'],
+                       's': ['a', 'a', 'a', 'b', 'c'],
+                       'na': [np.nan, np.nan, 3, 4, 1],
+                       'infs': [np.inf, np.nan, 3, 4, 1],
+                       'zero': [0, 0, 0, 0, 1],
+                       'zeros': [0, 0, 0, 0, 0]},
+                      columns=['a', 'b', 'c', 's', 'na', 'infs', 'zero', 'zeros'])
+
     # Check one or two dataframes, then just check the colnames
-    
 
     # Columns
 
-    droppedNaN = ut.drop(data = df, 
-                         axis = 1,
-                         value = 'NaN',
-                         thresh = 0,
-                         direction = '>')
+    droppedNaN = ut.drop(data=df,
+                         axis=1,
+                         value='NaN',
+                         thresh=0,
+                         direction='>')
 
-    assert (droppedNaN.columns == ['a', 'b','c', 's','na','infs','zero','zeros']).all()
-    assert droppedNaN.shape == (3,8)
-    assert (droppedNaN.index.values == np.asarray([2,3,4])).all()
+    assert (droppedNaN.columns == ['a', 'b', 'c',
+            's', 'na', 'infs', 'zero', 'zeros']).all()
+    assert droppedNaN.shape == (3, 8)
+    assert (droppedNaN.index.values == np.asarray([2, 3, 4])).all()
 
+    droppedinf = ut.drop(data=df,
+                         axis=1,
+                         value='inf',
+                         thresh=0,
+                         direction='>')
 
-    droppedinf = ut.drop(data = df, 
-                         axis = 1,
-                         value = 'inf',
-                         thresh = 0,
-                         direction = '>')
+    assert droppedinf.shape == (4, 8)
+    assert (droppedinf.index.values == np.asarray([1, 2, 3, 4])).all()
 
-    assert droppedinf.shape == (4,8)
-    assert (droppedinf.index.values == np.asarray([1,2,3,4])).all()
+    dropped0 = ut.drop(data=df,
+                       axis=1,
+                       value=0,
+                       # more than one appearance of 0 (8 columns)
+                       thresh=1.5 / 8,
+                       direction='>')
 
-    dropped0 = ut.drop(data = df, 
-                         axis = 1,
-                         value = 0,
-                         thresh = 1.5/8, # more than one appearance of 0 (8 columns)
-                         direction = '>')
-
-    assert dropped0.shape == (1,8)
+    assert dropped0.shape == (1, 8)
     assert (dropped0.index.values == np.asarray([4])).all()
 
+    droppedany = ut.drop(data=df,
+                         axis=1,
+                         value='any',
+                         thresh=2. / 8,  # more than two appearances of any value
+                         direction='>')
 
-    droppedany = ut.drop(data = df, 
-                         axis = 1,
-                         value = 'any',
-                         thresh = 2./8, # more than two appearances of any value
-                         direction = '>')
+    assert droppedany.shape == (2, 8)
+    assert (droppedany.index.values == np.asarray([0, 4])).all()
 
-    assert droppedany.shape == (2,8)
-    assert (droppedany.index.values == np.asarray([0,4])).all()
-    
-    droppednone = ut.drop(data = df, 
-                         axis = 1,
-                         value = 7,
-                         thresh = 1,
-                         direction = '>')
+    droppednone = ut.drop(data=df,
+                          axis=1,
+                          value=7,
+                          thresh=1,
+                          direction='>')
 
-    assert droppednone.shape == (5,8)
-    assert (droppednone.index.values == np.asarray([0,1,2,3,4])).all()
+    assert droppednone.shape == (5, 8)
+    assert (droppednone.index.values == np.asarray([0, 1, 2, 3, 4])).all()
 
     # include
-    droppedinc = ut.drop(data = df, 
-                         axis = 1,
-                         value = 'any',
-                         thresh = 2./8, # more than two appearances of any value
-                         direction = '>',
-                         include = [1,2])
+    droppedinc = ut.drop(data=df,
+                         axis=1,
+                         value='any',
+                         thresh=2. / 8,  # more than two appearances of any value
+                         direction='>',
+                         include=[1, 2])
 
-    assert droppedinc.shape == (3,8)
-    assert (droppedinc.index.values == np.asarray([0,3,4])).all()
-    
+    assert droppedinc.shape == (3, 8)
+    assert (droppedinc.index.values == np.asarray([0, 3, 4])).all()
+
     # # include
-    # droppedinc = ut.drop(data = df, 
+    # droppedinc = ut.drop(data = df,
     #                      axis = 1,
     #                      value = 'NaN',
     #                      thresh = 0,
@@ -169,14 +171,3 @@ def test_drop_DF_rows():
     #                      include = ['infs'])
 
     # assert (droppedinc.columns == ['a', 'b','c', 's','na','zero','zeros']).all()
-
-
-
-
-
-
-
-
-
-
-
--- tests/test_pandas_makes_up.py
+++ tests/test_pandas_makes_up.py
@@ -4,85 +4,90 @@ import utipy as ut
 import numpy as np
 import pandas as pd
 
+
 def test_makes_up_Series():
 
-	x0 = pd.Series([0,0,0,0,0])
-	xNaN = pd.Series([np.nan, np.nan, np.nan, np.nan, np.nan])
-	xinf = pd.Series([np.inf, np.inf, np.inf, np.inf, np.inf])
-	x6 = pd.Series([1,2,3,4,5,6,6,6,6,6])
-	
-	assert ut.makes_up(x0, 0, thresh = 1, direction = '==')
-	assert not ut.makes_up(x0, 0, thresh = 1, direction = '<')
-	assert ut.makes_up(x0, 0, thresh = 0.5, direction = '>')
+    x0 = pd.Series([0, 0, 0, 0, 0])
+    xNaN = pd.Series([np.nan, np.nan, np.nan, np.nan, np.nan])
+    xinf = pd.Series([np.inf, np.inf, np.inf, np.inf, np.inf])
+    x6 = pd.Series([1, 2, 3, 4, 5, 6, 6, 6, 6, 6])
 
-	assert ut.makes_up(xNaN, 'NaN', thresh = 1, direction = '==')
-	assert not ut.makes_up(xNaN, 'NaN', thresh = 1, direction = '<')
-	assert ut.makes_up(xNaN, 'NaN', thresh = 0.5, direction = '>')
+    assert ut.makes_up(x0, 0, thresh=1, direction='==')
+    assert not ut.makes_up(x0, 0, thresh=1, direction='<')
+    assert ut.makes_up(x0, 0, thresh=0.5, direction='>')
 
-	assert ut.makes_up(xinf, 'inf', thresh = 1, direction = '==')
-	assert not ut.makes_up(xinf, 'inf', thresh = 1, direction = '<')
-	assert ut.makes_up(xinf, 'inf', thresh = 0.5, direction = '>')
+    assert ut.makes_up(xNaN, 'NaN', thresh=1, direction='==')
+    assert not ut.makes_up(xNaN, 'NaN', thresh=1, direction='<')
+    assert ut.makes_up(xNaN, 'NaN', thresh=0.5, direction='>')
 
-	assert ut.makes_up(xinf, 'any', thresh = 1, direction = '==')
-	assert not ut.makes_up(xinf, 'any', thresh = 1, direction = '<')
-	assert ut.makes_up(xinf, 'any', thresh = 0.5, direction = '>')
+    assert ut.makes_up(xinf, 'inf', thresh=1, direction='==')
+    assert not ut.makes_up(xinf, 'inf', thresh=1, direction='<')
+    assert ut.makes_up(xinf, 'inf', thresh=0.5, direction='>')
 
-	assert not ut.makes_up(x6, 6, thresh = 1, direction = '==')
-	assert ut.makes_up(x6, 6, thresh = 1, direction = '<')
-	assert not ut.makes_up(x6, 6, thresh = 0.5, direction = '>')
-	assert not ut.makes_up(x6, 6, thresh = 0.5, direction = '<')
-	assert ut.makes_up(x6, 6, thresh = 0.5, direction = '==')
-	assert ut.makes_up(x6, 6, thresh = 0.3, direction = '>')
+    assert ut.makes_up(xinf, 'any', thresh=1, direction='==')
+    assert not ut.makes_up(xinf, 'any', thresh=1, direction='<')
+    assert ut.makes_up(xinf, 'any', thresh=0.5, direction='>')
+    assert ut.makes_up(xNaN, 'any', thresh=0.5, direction='>')
+
+    assert not ut.makes_up(x6, 6, thresh=1, direction='==')
+    assert ut.makes_up(x6, 6, thresh=1, direction='<')
+    assert not ut.makes_up(x6, 6, thresh=0.5, direction='>')
+    assert not ut.makes_up(x6, 6, thresh=0.5, direction='<')
+    assert ut.makes_up(x6, 6, thresh=0.5, direction='==')
+    assert ut.makes_up(x6, 6, thresh=0.3, direction='>')
 
 
 def test_makes_up_ndarray():
 
-	x0 = np.asarray([0,0,0,0,0])
-	xNaN = np.asarray([np.nan, np.nan, np.nan, np.nan, np.nan])
-	xinf = np.asarray([np.inf, np.inf, np.inf, np.inf, np.inf])
-	x6 = np.asarray([1,2,3,4,5,6,6,6,6,6])
-	
-	assert ut.makes_up(x0, 0, thresh = 1, direction = '==')
-	assert not ut.makes_up(x0, 0, thresh = 1, direction = '<')
-	assert ut.makes_up(x0, 0, thresh = 0.5, direction = '>')
+    x0 = np.asarray([0, 0, 0, 0, 0])
+    xNaN = np.asarray([np.nan, np.nan, np.nan, np.nan, np.nan])
+    xinf = np.asarray([np.inf, np.inf, np.inf, np.inf, np.inf])
+    x6 = np.asarray([1, 2, 3, 4, 5, 6, 6, 6, 6, 6])
 
-	assert ut.makes_up(xNaN, 'NaN', thresh = 1, direction = '==')
-	assert not ut.makes_up(xNaN, 'NaN', thresh = 1, direction = '<')
-	assert ut.makes_up(xNaN, 'NaN', thresh = 0.5, direction = '>')
+    assert ut.makes_up(x0, 0, thresh=1, direction='==')
+    assert not ut.makes_up(x0, 0, thresh=1, direction='<')
+    assert ut.makes_up(x0, 0, thresh=0.5, direction='>')
 
-	assert ut.makes_up(xinf, 'inf', thresh = 1, direction = '==')
-	assert not ut.makes_up(xinf, 'inf', thresh = 1, direction = '<')
-	assert ut.makes_up(xinf, 'inf', thresh = 0.5, direction = '>')
+    assert ut.makes_up(xNaN, 'NaN', thresh=1, direction='==')
+    assert not ut.makes_up(xNaN, 'NaN', thresh=1, direction='<')
+    assert ut.makes_up(xNaN, 'NaN', thresh=0.5, direction='>')
 
-	assert not ut.makes_up(x6, 6, thresh = 1, direction = '==')
-	assert ut.makes_up(x6, 6, thresh = 1, direction = '<')
-	assert not ut.makes_up(x6, 6, thresh = 0.5, direction = '>')
-	assert not ut.makes_up(x6, 6, thresh = 0.5, direction = '<')
-	assert ut.makes_up(x6, 6, thresh = 0.5, direction = '==')
-	assert ut.makes_up(x6, 6, thresh = 0.3, direction = '>')
+    assert ut.makes_up(xinf, 'inf', thresh=1, direction='==')
+    assert not ut.makes_up(xinf, 'inf', thresh=1, direction='<')
+    assert ut.makes_up(xinf, 'inf', thresh=0.5, direction='>')
+
+    assert ut.makes_up(xNaN, 'any', thresh=0.5, direction='>')
+
+    assert not ut.makes_up(x6, 6, thresh=1, direction='==')
+    assert ut.makes_up(x6, 6, thresh=1, direction='<')
+    assert not ut.makes_up(x6, 6, thresh=0.5, direction='>')
+    assert not ut.makes_up(x6, 6, thresh=0.5, direction='<')
+    assert ut.makes_up(x6, 6, thresh=0.5, direction='==')
+    assert ut.makes_up(x6, 6, thresh=0.3, direction='>')
+
 
 def test_makes_up_list():
 
-	x0 = [0,0,0,0,0]
-	xNaN = [np.nan, np.nan, np.nan, np.nan, np.nan]
-	xinf = [np.inf, np.inf, np.inf, np.inf, np.inf]
-	x6 = [1,2,3,4,5,6,6,6,6,6]
-	
-	assert ut.makes_up(x0, 0, thresh = 1, direction = '==')
-	assert not ut.makes_up(x0, 0, thresh = 1, direction = '<')
-	assert ut.makes_up(x0, 0, thresh = 0.5, direction = '>')
+    x0 = [0, 0, 0, 0, 0]
+    xNaN = [np.nan, np.nan, np.nan, np.nan, np.nan]
+    xinf = [np.inf, np.inf, np.inf, np.inf, np.inf]
+    x6 = [1, 2, 3, 4, 5, 6, 6, 6, 6, 6]
 
-	assert ut.makes_up(xNaN, 'NaN', thresh = 1, direction = '==')
-	assert not ut.makes_up(xNaN, 'NaN', thresh = 1, direction = '<')
-	assert ut.makes_up(xNaN, 'NaN', thresh = 0.5, direction = '>')
+    assert ut.makes_up(x0, 0, thresh=1, direction='==')
+    assert not ut.makes_up(x0, 0, thresh=1, direction='<')
+    assert ut.makes_up(x0, 0, thresh=0.5, direction='>')
 
-	assert ut.makes_up(xinf, 'inf', thresh = 1, direction = '==')
-	assert not ut.makes_up(xinf, 'inf', thresh = 1, direction = '<')
-	assert ut.makes_up(xinf, 'inf', thresh = 0.5, direction = '>')
+    assert ut.makes_up(xNaN, 'NaN', thresh=1, direction='==')
+    assert not ut.makes_up(xNaN, 'NaN', thresh=1, direction='<')
+    assert ut.makes_up(xNaN, 'NaN', thresh=0.5, direction='>')
 
-	assert not ut.makes_up(x6, 6, thresh = 1, direction = '==')
-	assert ut.makes_up(x6, 6, thresh = 1, direction = '<')
-	assert not ut.makes_up(x6, 6, thresh = 0.5, direction = '>')
-	assert not ut.makes_up(x6, 6, thresh = 0.5, direction = '<')
-	assert ut.makes_up(x6, 6, thresh = 0.5, direction = '==')
-	assert ut.makes_up(x6, 6, thresh = 0.3, direction = '>')
+    assert ut.makes_up(xinf, 'inf', thresh=1, direction='==')
+    assert not ut.makes_up(xinf, 'inf', thresh=1, direction='<')
+    assert ut.makes_up(xinf, 'inf', thresh=0.5, direction='>')
+
+    assert not ut.makes_up(x6, 6, thresh=1, direction='==')
+    assert ut.makes_up(x6, 6, thresh=1, direction='<')
+    assert not ut.makes_up(x6, 6, thresh=0.5, direction='>')
+    assert not ut.makes_up(x6, 6, thresh=0.5, direction='<')
+    assert ut.makes_up(x6, 6, thresh=0.5, direction='==')
+    assert ut.makes_up(x6, 6, thresh=0.3, direction='>')
