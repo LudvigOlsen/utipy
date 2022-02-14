60951e7e5dea00790621a480db65f91e60e2af39
Ludvig <ludvig@clin.au.dk>
2022-02-14 17:59:39 +0100

    Adds docs and typing for time formatter

--- utipy/time/format_time.py
+++ utipy/time/format_time.py
@@ -1,14 +1,25 @@
 
+from numbers import Number
 import numpy as np
 
 
-def format_time_hhmmss(t):
+def format_time_hhmmss(t: int) -> str:
     """
-    Format a count of seconds into HH:MM:SS where HH is allowed to be >24.
+    Format a count of seconds into hh:mm:ss where hh is allowed to be >24.
 
     NOTE: As `time.strftime(fmt, time.gmtime(t))` is not perfect when dealing with multiday running times
     we just wrote our own formatting that allows >24 hours. We don't need to split into days.
     Only 2x in speed, so does not affect running time in practice. (5000 runs < 0.1s)
+
+    Parameters
+    ----------
+    t : int
+        Timepoint as found with `time.time()`.
+
+    Returns
+    -------
+    str
+        The timepoint formatted as a hh:mm:ss string.
     """
     # Hours
     hours = np.floor(t / 60 / 60)
@@ -24,7 +35,20 @@ def format_time_hhmmss(t):
     return f"{_format_part(hours)}:{_format_part(mins)}:{_format_part(secs)}"
 
 
-def _format_part(t):
+def _format_part(t: Number) -> str:
+    """
+    Format integer-like number as 2-digit string (either hh, mm, or ss).
+
+    Parameters
+    ----------
+    t : Number
+        A 1- or 2-digit number (after conversion to integer).
+
+    Returns
+    -------
+    str
+        A two-character string. From "00" -> "99".
+    """
     t = int(t)
     if t == 0:
         return "00"
