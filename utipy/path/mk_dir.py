b04a705c4c41dc407f72ff7053d237c6ea76bef2
Ludvig <ludvig@clin.au.dk>
2022-02-14 17:01:12 +0100

    Adds docs, types and improvements to mk_dir

--- utipy/path/mk_dir.py
+++ utipy/path/mk_dir.py
@@ -1,10 +1,31 @@
 
 import pathlib
+from typing import Union
+
+# TODO Convert to logging, so it can be turned off when not debugging
 
 
-def mk_dir(path, arg_name='out_path', verbose=True):
+def mk_dir(path: Union[str, pathlib.Path], arg_name: Union[str, None] = 'out_path', verbose: bool = True):
+    """
+    Make directory if it doesn't exist.
+
+    Parameters
+    ----------
+    path : str or `pathlib.Path`
+        Path to directory to make.
+    arg_name : str or None
+        Name of path argument/variable for message 
+        when creating a folder and `verbose` is `True`.
+    verbose: bool
+        Whether to print a message when creating a folder.
+    """
     path = pathlib.Path(path)
     if verbose and not path.exists():
+        if arg_name is None or not arg_name:
+            arg_name = ""
+        else:
+            arg_name = f"`{arg_name}` "
         print(
-            f"`{arg_name}` directory does not exist and will be created: {path.resolve()}")
+            f"{arg_name}directory does not exist and will be created: "
+            f"{path.resolve()}")
     path.mkdir(parents=True, exist_ok=True)
