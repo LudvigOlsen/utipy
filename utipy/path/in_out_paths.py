6dd5b3a9894260605e6ba50b884f626520bd99e4
Ludvig <ludvig@clin.au.dk>
2022-02-14 16:48:27 +0100

    Add documentation, types and __eq__ for InOutPaths class

--- utipy/path/in_out_paths.py
+++ utipy/path/in_out_paths.py
@@ -2,12 +2,12 @@
 import os
 import pathlib
 from sys import path
+from typing import Union
 
 from utipy.path.mk_dir import mk_dir
 from utipy.path.prepare_paths import prepare_in_out_paths
 
 
-# TODO Add docs
 # TODO Add tests
 # TODO Allow keys to have a list of paths?
 
@@ -22,21 +22,124 @@ class InOutPaths:
     ]
 
     def __init__(self,
-                 in_files=None,
-                 in_dirs=None,
-                 out_files=None,
-                 out_dirs=None,
-                 tmp_files=None,
-                 allow_none=False,
-                 allow_overwriting=True,
-                 allow_duplicates_in_files=False,
-                 allow_duplicates_in_dirs=True,
-                 allow_duplicates_out_files=False,
-                 allow_duplicates_out_dirs=True,
-                 allow_duplicates_tmp_files=True,
-                 print_note="",
+                 in_files: dict = None,
+                 in_dirs: dict = None,
+                 out_files: dict = None,
+                 out_dirs: dict = None,
+                 tmp_files: dict = None,
+                 allow_none: bool = False,
+                 allow_overwriting: bool = True,
+                 allow_duplicates_in_files: bool = False,
+                 allow_duplicates_in_dirs: bool = True,
+                 allow_duplicates_out_files: bool = False,
+                 allow_duplicates_out_dirs: bool = True,
+                 allow_duplicates_tmp_files: bool = True,
+                 print_note: bool = "",
                  ) -> None:
+        """
+        Collection of path collections for keeping track of in- and output paths,
+        be it files or directories. Has relevant checks, such as duplication and
+        existence, and allows printing a summary of the paths. Paths are converted to 
+        `pathlib.Path` objects and missing directories can be created.
 
+        Note: Even when `allow_overwriting` is enabled, 
+        `in_files` and `out_files` cannot contain the same paths.
+
+        Keys must be unique across the path dictionaries.
+
+        Parameters
+        ----------
+        in_files : dict
+            Dict with named paths to input files, mapping `argument name -> input filepath`.
+            A path can also be "-" when streaming input. 
+            This value will remain a string and will not be checked
+            for duplication, existence, etc.
+        in_dirs : dict
+            Dict with named paths to input directories, mapping `argument name -> input directory path`.
+        out_files : dict
+            Dict with named paths to output files, mapping `argument name -> output filepath`.
+        out_dirs : dict
+            Dict with named paths to output directories, mapping `argument name -> output directory path`.
+        tmp_files : dict
+            Dict with named paths to temporary files, mapping `(argument) name -> temporary filepath`.
+                The names do not have to be argument names.
+        allow_none : bool
+            Whether to allow paths (i.e. dict values) to be `None`,
+            in which case they will be ignored.
+        allow_overwriting : bool
+            Whether to allow `out_files` paths to already exist.
+        allow_duplicates_in_files : bool
+            Whether to allow duplicate values (i.e. paths) in `in_files`.
+        allow_duplicates_in_dirs : bool
+            Whether to allow duplicate values (i.e. paths) in `in_dirs`.
+        allow_duplicates_out_files : bool
+            Whether to allow duplicate values (i.e. paths) in `out_files`.
+        allow_duplicates_out_dirs : bool
+            Whether to allow duplicate values (i.e. paths) in `out_dirs`.
+        print_note : string
+            String to add to end of `__str__` method.
+            That is a suffix added when printing the collection.
+
+        Examples
+        --------
+
+        Uncomment code to run.
+
+        # # Usually the paths come from input arguments or variables
+        # # Additional paths can be added later with `set_path()`.
+
+        # # Initialize the collection of path collections
+        # paths = InOutPaths(
+        #     in_files={
+        #         "in_file": "../dir1/dir2/john.csv",
+        #         "stream_in": "-"
+        #     },
+        #     in_dirs={
+        #         "in_dir": "../dir1/dir2/",
+        #     },
+        #     out_files={
+        #         "out_file": "../dir1/dir2/output/no_john.csv"
+        #     },
+        #     out_dirs={
+        #         "out_path": "../dir1/dir2/output/"
+        #     }
+        # )
+
+        # # Add additional path
+        # # Reruns checks to ensure consistency
+        # paths.set_path(
+        #     name="in_file_2",
+        #     path="../dir1/dir2/dennis.csv",
+        #     collection="in_files"
+        # )
+        # # Or set multiple at a time to avoid rerunning checks unnecessarily
+        # paths.set_paths(
+        #     paths={
+        #         "out_file_2": "../dir1/dir2/output/no_dennis.csv"
+        #         "out_file_3": "../dir1/dir2/output/readme.txt"
+        #     }, 
+        #     collection="out_files"
+        # )
+
+        # # Create the output directories that do not exist
+        # paths.mk_output_dirs(collection="out_dirs")
+
+        # # Get a path
+        # paths["in_file"] # or
+        # paths.get_path(name="in_file", as_str=False, raise_on_fail=True)
+
+        # # Remove file from disk
+        # paths.rm_file(name="in_file")
+
+        # # Update collection with another `InOutPaths` collection
+        # # The sub collections are dicts, why this is just dict.update() 
+        # # on each sub collection
+        # paths.update(other=other_paths)
+
+        # # Find the combinations of keys and paths in the collections of this object
+        # # that are not in the collections of another object.
+        # paths.difference(other=other_paths):
+        """
         self.all_paths = None
         self._collections = {
             "in_files": in_files,
@@ -61,16 +164,42 @@ class InOutPaths:
 
     @property
     def npaths(self):
+        """
+        Total number of stored paths.
+
+        Returns
+        -------
+        Int
+        """
         return sum(self.collection_sizes().values())
 
     @property
     def collection_sizes(self):
+        """
+        Sizes of collections (size: number of paths).
+
+        Returns
+        -------
+        Dict mapping collection name -> number of paths.
+        """
         return {
             coll: self.get_collection_size(coll)
             for coll in InOutPaths.COLLECTION_NAMES
         }
 
-    def get_collection_size(self, collection):
+    def get_collection_size(self, collection: str):
+        """
+        Get size (number of paths) of a given collection.
+
+        Parameters
+        ----------
+        collection : str
+            Name of collection to get size of.
+
+        Returns
+        -------
+        Int
+        """
         coll = self.get_collection(collection)
         if coll is None:
             return 0
@@ -78,20 +207,45 @@ class InOutPaths:
 
     # Getters and setters
 
-    def get_collection(self, collection):
-        assert isinstance(collection, str)
-        self._check_collection_name(collection)
-        return self._collections[collection]
+    def get_collection(self, name: str):
+        """
+        Get collection of paths.
 
-    def set_collection(self, name, coll):
+        Parameters
+        ----------
+        name : str
+            Name of collection to get.
+
+        Returns
+        -------
+        Dict mapping `name -> path`.
+        """
+        assert isinstance(name, str)
+        self._check_collection_name(name)
+        return self._collections[name]
+
+    def set_collection(self, name: str, collection: dict):
+        """
+        Set collection of paths.
+
+        See also: `update()`.
+
+        Parameters
+        ----------
+        name : str
+            Name of collection to set.
+        collection : dict 
+            Collection of paths as dict, mapping `name -> path`. 
+            Replaces existing dict.
+        """
         # NOTE: Avoid internal use
         # as it may cause an infinite loop with
         # `self._prepare_paths()`
-        self._set_collection(name=name, coll=coll)
+        self._set_collection(name=name, coll=collection)
         # Ensure consistency
         self._prepare_paths()
 
-    def _set_collection(self, name, coll):
+    def _set_collection(self, name: str, coll: Union[dict, None]):
         assert isinstance(name, str)
         assert coll is None or isinstance(coll, dict)
         if name not in InOutPaths.COLLECTION_NAMES:
@@ -99,7 +253,7 @@ class InOutPaths:
                 f"`name` was not one of the allowed collection names: {InOutPaths.COLLECTION_NAMES}")
         self._collections[name] = coll
 
-    def _update_collection(self, paths, collection):
+    def _update_collection(self, paths: dict, collection: str):
         assert isinstance(paths, dict)
         assert isinstance(collection, str)
         self._check_collection_name(collection)
@@ -107,7 +261,7 @@ class InOutPaths:
             self._set_collection(name=collection, coll=paths)
         self._collections[collection].update(paths)
 
-    def __getitem__(self, name):
+    def __getitem__(self, name: str):
         """
         Get path from its key:
 
@@ -115,7 +269,24 @@ class InOutPaths:
         """
         return self.get_path(name=name)
 
-    def get_path(self, name, as_str=False, raise_on_fail=True):
+    def get_path(self, name: str, as_str: bool = False, raise_on_fail: bool = True):
+        """
+        Get a path.
+
+        Parameters
+        ----------
+        name : str
+            Name of path to get.
+        as_str : bool
+            Whether to convert the path to string (default is as `pathlib.Path`).
+        raise_on_fail : bool
+            Whether to raise an error when the path does not exist.
+            When `False`, it defaults to `None`.
+
+        Returns
+        -------
+        `pathlib.Path`, str or `None`
+        """
         if name in self.all_paths:
             return self._format_path_out(path=self.all_paths[name], as_str=as_str)
         if raise_on_fail:
@@ -123,7 +294,20 @@ class InOutPaths:
                 f"{name} was not a known key in any of the path collections.")
         return None
 
-    def set_path(self, name, path, collection):
+    def set_path(self, name: str, path: Union[str, pathlib.Path], collection: str):
+        """
+        Set a path in a collection.
+
+        Parameters
+        ----------
+        name : str
+            Name of path for collection dict.
+        path : str or `pathlib.Path` 
+            The path to set.
+        collection : str
+            Name of collection to set the path in.
+            When a name already exist it is overwritten.
+        """
         # NOTE: Avoid internal use
         # as it may cause an infinite loop
         assert isinstance(name, str)
@@ -134,7 +318,18 @@ class InOutPaths:
         # Ensure consistency
         self._prepare_paths()
 
-    def set_paths(self, paths, collection):
+    def set_paths(self, paths: dict, collection: str):
+        """
+        Set one or more paths in a collection.
+
+        Parameters
+        ----------
+        paths : dict
+            Dict mapping `path name -> path`.
+            Paths can be of type str or `pathlib.Path`.
+        collection : str
+            Name of collection to set the path in.
+        """
         # NOTE: Avoid internal use
         # as it may cause an infinite loop
         self._update_collection(paths=paths, collection=collection)
@@ -146,10 +341,11 @@ class InOutPaths:
 
     def check_paths(self):
         """
-        Call path checks manually. These checks are automatically 
-        called after each mutation with setter methods, but when
-        overwriting attributes (like check settings) manually, we may need 
-        to call it externally.
+        Call path checks manually. 
+
+        These checks are automatically called after each mutation with 
+        setter methods, but when overwriting attributes (like check settings) 
+        manually, we may need to call it externally.
         """
         self._prepare_paths()
 
@@ -179,7 +375,38 @@ class InOutPaths:
         self._set_collection("tmp_files", tmp_files)
         self.all_paths = all_paths
 
-    def mk_output_dirs(self, collection=None, verbose=True):
+    def mk_output_dir(self, name: str, verbose: bool = True):
+        """
+        Create non-existing output directory for a given path.
+
+        For filepaths, it creates the directory the file is located in.
+
+        Parameters
+        ----------
+        name : str
+            Name of path to create output directory for.
+        verbose : bool
+            Whether to print message on creation of a new folder.
+        """
+        path = self.get_path(name=name)
+        dir_path = pathlib.Path(path).parent
+        mk_dir(path=dir_path, arg_name=name, verbose=verbose)
+
+    def mk_output_dirs(self, collection: str = None, verbose: bool = True):
+        """
+        Create non-existing output directories.
+
+        For filepaths, it creates the directory the file is located in.
+
+        Parameters
+        ----------
+        collection : str
+            Name of collection to create output directories for.
+                One of: ('out_dirs', 'out_files', 'tmp_files')
+            When `None`, directories are created for all three collections.
+        verbose : bool
+            Whether to print message on creation of a new folder.
+        """
 
         # Find which collections to create output dirs for
         mkdirs_for_out_files = True
@@ -226,19 +453,37 @@ class InOutPaths:
                 dir_path = pathlib.Path(v).parent
                 mk_dir(path=dir_path, arg_name=k, verbose=verbose)
 
-    def rm_file(self, name):
+    def rm_file(self, name: str, raise_on_fail: bool = True):
         """
-        Delete file from the disk.
+        Remove a file from disk.
+
+        Parameters
+        ----------
+        name : str
+            Name of path to a file to remove from disk.
+        raise_on_fail : bool
+            Whether to raise an error when the path does not exist.
         """
         path = self[name]
         if path is None:
             raise ValueError(f"Path object for `{name}` was `None`.")
         if not path.is_file():
-            raise RuntimeError(
-                f"Path for `{name}` was not an existing file: {path}")
-        os.remove(str(path))
+            if raise_on_fail:
+                raise RuntimeError(
+                    f"Path for `{name}` was not an existing file: {path}")
+        else:
+            os.remove(str(path))
 
-    def update(self, other):
+    def update(self, other: object):
+        """
+        Update with paths from another `InOutPaths` collection.
+
+        Parameters
+        ----------
+        other : `InOutPaths` instance
+            Another `InOutPaths` instance with paths.
+            Each sub collection is dict-updated one at a time.
+        """
         assert isinstance(other, InOutPaths)
         for coll_name in InOutPaths.COLLECTION_NAMES:
             self._update_collection(
@@ -249,11 +494,17 @@ class InOutPaths:
         # Ensure consistency
         self._prepare_paths()
 
-    def difference(self, other):
+    def difference(self, other: object):
         """
         Find the combinations of keys and paths in the collections of this object
-        that are not in the collections of the other object.
+        that are not in the collections of the `other` object.
         Creates a new `InOutPaths` object with the settings from this object.
+
+        Parameters
+        ----------
+        other : `InOutPaths` instance
+            Another `InOutPaths` instance with paths.
+            Each sub collection is dict-updated one at a time.
         """
         assert isinstance(other, InOutPaths)
         diff_dicts = {}
@@ -277,10 +528,54 @@ class InOutPaths:
             tmp_files=diff_dicts["tmp_files"]
         )
 
-    def _new(self, in_files=None, in_dirs=None, out_files=None, out_dirs=None, tmp_files=None):
+    def __eq__(self, other: object) -> bool:
         """
-        Create new `InOutPaths` object with new collections but
-        keeping the rest of the settings.
+        Check equality of this `InOutPaths` instance and another.
+
+        Checks that all sub collections are the same. 
+        No other attributes are considered.
+
+        Parameters
+        ----------
+        other : `InOutPaths` instance
+            Another `InOutPaths` instance with paths.
+        """
+        assert isinstance(other, InOutPaths)
+        if self.npaths != other.npaths:
+            return False
+        for coll_name in InOutPaths.COLLECTION_NAMES:
+            this_coll = self.get_collection(collection=coll_name)
+            other_coll = other.get_collection(collection=coll_name)
+            if sum([other_coll is None, this_coll is None]) == 1:
+                return False
+            if this_coll != other_coll:
+                return False
+        return True
+
+    def _new(self, in_files: dict = None, in_dirs: dict = None, out_files: dict = None, out_dirs: dict = None, tmp_files: dict = None):
+        """
+        Create new `InOutPaths` object with new collections but keeping the rest of the settings.
+
+        Parameters
+        ----------
+        in_files : dict
+            Dict with named paths to input files, mapping `argument name -> input filepath`.
+            A path can also be "-" when streaming input. 
+            This value will remain a string and will not be checked
+            for duplication, existence, etc.
+        in_dirs : dict
+            Dict with named paths to input directories, mapping `argument name -> input directory path`.
+        out_files : dict
+            Dict with named paths to output files, mapping `argument name -> output filepath`.
+        out_dirs : dict
+            Dict with named paths to output directories, mapping `argument name -> output directory path`.
+        tmp_files : dict
+            Dict with named paths to temporary files, mapping `(argument) name -> temporary filepath`.
+                The names do not have to be argument names.
+
+        Returns
+        -------
+        `InOutPaths` instance
         """
         return InOutPaths(
             in_files=in_files,
@@ -298,16 +593,19 @@ class InOutPaths:
             print_note=self.print_note
         )
 
-    def _check_collection_name(self, name):
+    def _check_collection_name(self, name: str):
         if name not in InOutPaths.COLLECTION_NAMES:
             raise ValueError(f"Collection name was unknown: {name}.")
 
-    def _format_path_out(self, path, as_str):
+    def _format_path_out(self, path, as_str: bool):
         if as_str:
             path = str(path)
         return path
 
     def __str__(self) -> str:
+        """
+        Convert object to a string with the paths for each sub collection.
+        """
         max_paths_per_coll = 10
         lines = []
         lines.append("Input and output paths")
