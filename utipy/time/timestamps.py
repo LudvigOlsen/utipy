5958017bb5da5cd43e6bf9c77f2d2224dea07f02
Ludvig <ludvig@clin.au.dk>
2022-02-15 15:09:54 +0100

    Adds merge and update methods to Timestamps

--- utipy/time/timestamps.py
+++ utipy/time/timestamps.py
@@ -1,6 +1,8 @@
 
+
 import time
 from typing import Union
+import numpy as np
 
 from .format_time import format_time_hhmmss
 
@@ -17,6 +19,7 @@ class Timestamps:
         """
         self.timestamps = []
         self.name_to_idx = {}
+        self.idx_to_name = {}
 
     def __len__(self) -> int:
         """
@@ -68,9 +71,12 @@ class Timestamps:
             (Optional) Unique name to store the index of the timestamp with.
         """
         self._stamp()
-        if name in self.name_to_idx:
-            raise ValueError("`name` was already used. Use a unique name.")
-        self.name_to_idx[name] = len(self) - 1
+        if name is not None:
+            if name in self.name_to_idx:
+                raise ValueError("`name` was already used. Use a unique name.")
+            idx = len(self) - 1
+            self.name_to_idx[name] = idx
+            self.idx_to_name[idx] = name
 
     def get_stamp(self, idx: Union[int, None] = None, name: Union[str, None] = None, as_str: bool = True) -> Union[int, str]:
         """
@@ -108,21 +114,41 @@ class Timestamps:
 
     def get_stamp_idx(self, name: str) -> int:
         """
-        Get timestamp index from name of special timestamp.
+        Get timestamp index from name of timestamp.
 
         Parameters
         ----------
         name : str
-            Name used to store index of special timestamp with.
+            Name used to store index of timestamp with.
 
         Returns
         -------
         int
-            Index of special timestamp stored with `name`.
+            Index of timestamp stored with `name`.
         """
         if name not in self.name_to_idx:
             raise ValueError(f"`name` was not a known name: '{name}'.")
         return self.name_to_idx[name]
+
+    def get_stamp_name(self, idx: int) -> str:
+        """
+        Get timestamp name from index of timestamp.
+
+        Parameters
+        ----------
+        idx : int
+            Index of timestamp to get name for.
+
+        Returns
+        -------
+        str or None
+            Name of timestamp at the given index.
+            When no name was recorded, `None` is returned.
+        """
+        if np.abs(idx) > len(self) - 1:
+            raise ValueError(
+                f"`idx` was out of bounds: '{idx}'. Currently stores {len(self)} timestamps.")
+        return self.idx_to_name.get(idx, default=None)
 
     def took(self, start: Union[int, str] = -2, end: Union[int, str] = -1,
              as_str: bool = True, raise_negative: bool = True) -> Union[int, str]:
@@ -194,3 +220,98 @@ class Timestamps:
             end_idx=-1,
             as_str=as_str
         )
+
+    def update(self, other: object):
+        """
+        Update existing `Timestamps` collection with another `Timestamps` collection.
+        Combines the list of timestamps (sorted by time) and updates the name->idx and idx->name maps,
+        which is likely to change the indices.
+
+        When both collections have the same key in the `.name_to_idx` and `.idx_to_name` dicts,
+        the member from `other` is used.
+
+        Parameters
+        ----------
+        other : `Timestamps` object
+            Another `Timestamps` collection to combine with the current collection.
+        """
+        self.merge(other=other, suffix_identical_names=False)
+
+    def merge(self, other: object, suffix_identical_names: bool = True):
+        """
+        Merge this `Timestamps` collection with another `Timestamps` collection.
+        Combines the list of timestamps (sorted by time) and updates the name->idx and idx->name maps,
+        which is likely to change the indices.
+
+        By default, clashing names are suffixed with "_0" (this) and "_1" (other).
+        If this does not lead to unique names, the suffix increases by one ("_2", "_3", "_4", ...)
+        until it does.
+
+        Parameters
+        ----------
+        other : `Timestamps` object
+            Another `Timestamps` collection to combine with the current collection.
+        suffix_identical_names : bool
+            Whether to add a suffix ("_0", "_1", etc.) to clashing names
+            with an increasing count until names are unique.
+            When `False`, the dict members in `.name_to_idx` and `.idx_to_name` 
+            from `other` is used.
+        """
+        # Add the two timestamps lists together
+        # but as tuples with indices and an identifier
+        # of which collection it came from
+        combined_timestamps = \
+            _list_to_enumerated_tuple(
+                l=self.timestamps, id="this") + \
+            _list_to_enumerated_tuple(
+                l=other.timestamps, id="other")
+
+        # Sort by ascending time
+        combined_timestamps = sorted(combined_timestamps, key=lambda x: x[0])
+
+        # Update name->index maps
+        colls = {"this": self, "other": other}
+        for new_idx, (_, old_idx, coll_id) in enumerate(combined_timestamps):
+            coll = colls[coll_id]
+            name = coll.idx_to_name.get(old_idx, default=None)
+            if name is not None:
+                coll.name_to_idx[name] = new_idx
+
+        # Handle clashing names
+        # Either by suffixing or overwriting
+        if suffix_identical_names:
+            # Find the clashing names
+            all_names = list(self.name_to_idx.keys()) + \
+                list(other.name_to_idx.keys())
+            duplicate_names = set(self.name_to_idx.keys()).intersection(
+                other.name_to_idx.keys())
+            if duplicate_names:
+                for coll in [self, other]:
+                    for name in duplicate_names:
+                        new_name = _create_unique_name(name, all_names)
+                        all_names += [new_name]
+                        coll.name_to_idx[new_name] = coll.name_to_idx.pop(name)
+
+        # Update the dict
+        self.name_to_idx.update(other.name_to_idx)
+
+        # Create idx_to_name dict
+        self.idx_to_name = {v: k for k, v in self.name_to_idx.items()}
+
+
+def _list_to_enumerated_tuple(l, identifier):
+    def make_tuple(t, i, identifier):
+        if identifier is None:
+            return (t, i)
+        return (t, i, identifier)
+    return [make_tuple(t, i, identifier) for i, t in enumerate(l)]
+
+
+def _create_unique_name(name, l):
+    # Add underscore
+    name += "_"
+    counter = 0
+    while name + str(counter) in l:
+        counter += 1
+    return name + str(counter)
+        
