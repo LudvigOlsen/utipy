02442d785945d85c864bfabae632acdfbebf5569
Ludvig <ludvig@clin.au.dk>
2022-02-15 12:30:30 +0100

    Docs

--- utipy/utils/extended_describe.py
+++ utipy/utils/extended_describe.py
@@ -8,6 +8,10 @@ from utipy.measures.iqr import _iqr
 # For pandas objects
 
 def _extended_describe(Series):
+    """
+    Adds median and IQR to `pandas.Series.describe()`.
+    Only works for numeric series.
+    """
     desc = Series.describe()
     desc['median'] = np.median(Series)
     desc['IQR'] = _iqr(Series)
