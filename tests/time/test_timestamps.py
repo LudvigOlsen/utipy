8b386dccc7e758881e04e3422a4fffc495a79928
Ludvig <ludvig@clin.au.dk>
2022-02-16 10:47:33 +0100

    More testing of Timestamps

--- tests/time/test_timestamps.py
+++ tests/time/test_timestamps.py
@@ -1,11 +1,11 @@
 
 
 from time import sleep
-
+import pytest
 from utipy.time.timestamps import Timestamps
 
 
-def test_timestamps_number_of_stamps():
+def test_timestamps():
     stamper = Timestamps()
     stamper.stamp()
     sleep(0.001)
@@ -15,6 +15,8 @@ def test_timestamps_number_of_stamps():
     sleep(0.001)
     stamper.stamp(name="fourth")
     total_time = stamper.get_total_time(as_str=True)
+    assert stamper.took(start=0, end=-1, as_str=False) == \
+        stamper.get_total_time(as_str=False)
     assert len(stamper) == 4
     assert "second" in stamper.name_to_idx
     assert stamper.name_to_idx["second"] == 1
@@ -23,6 +25,16 @@ def test_timestamps_number_of_stamps():
     assert stamper.name_to_idx["fourth"] == 3
     assert stamper.idx_to_name[3] == "fourth"
     assert total_time == "00:00:00"  # NOTE: Could technically fail on rare occasions
+
+    assert stamper.took(start=0, end=1, as_str=False) == \
+        stamper[1] - stamper[0]
+    assert stamper.took(start=1, end=3, as_str=False) == \
+        stamper["fourth"] - stamper["second"]
+    assert stamper.took(start="second", end="fourth", as_str=False) == \
+        stamper["fourth"] - stamper["second"]
+
+    with pytest.raises(KeyError):
+        stamper["notaknownname"]
 
 
 def test_timestamps_merge():
@@ -75,3 +87,40 @@ def test_timestamps_merge():
     assert stamper_1.get_stamp_idx("third") == 6
     assert stamper_1.get_stamp_name(1) == "second_0"
     assert stamper_1.get_stamp_name(6) == "third"
+
+
+def test_timestamps_update():
+    stamper_1 = Timestamps()
+    stamper_2 = Timestamps()
+
+    # Intertwined stamping
+
+    stamper_1.stamp()
+    sleep(0.001)
+    stamper_1.stamp(name="second")
+    sleep(0.001)
+
+    stamper_2.stamp()
+    sleep(0.001)
+    stamper_2.stamp(name="second")  # Clashes
+    sleep(0.001)
+
+    stamper_1.stamp()
+    sleep(0.001)
+    stamper_1.stamp(name="fourth")
+    sleep(0.001)
+
+    stamper_2.stamp(name="third")
+    sleep(0.001)
+    stamper_2.stamp()
+
+    # Overwrites clashing names
+    stamper_1.update(stamper_2)
+
+    assert len(stamper_1) == 8
+    assert len(stamper_2) == 4
+    assert stamper_1.name_to_idx == {
+        "second": 3,
+        "fourth": 5,
+        "third": 6
+    }
